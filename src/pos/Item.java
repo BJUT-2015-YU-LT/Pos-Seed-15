3bb1290d46cba2aee272f7a3335b42e520d33977
hanchao <1960492622@qq.com>
2016-01-15 13:37:43 +0800

    需求六 最终

--- src/pos/Item.java
+++ src/pos/Item.java
@@ -1,29 +1,50 @@
 package pos;
-
 public class Item{
-    String barcode;     // �商品编码�
-    String name;	     //�商品名�
-    String unit;        //��单位
-    Double  price;    //商品单价
-    int num;           //商品数量
-    Double total=0.0;  //总价
-    public Item(String b, String n, String u, Double p){
-        barcode=b;
-        name=n;
-        unit=u;
-        price=p;
-        num=1;
-        total=p;
-    }
-    public void addItemNum(){
-        total+=price;
-        num++;
-    }
-    public String toString(){
-        return ("名称："+name+"，数量："+num+unit+"，单价："
-                +price+"(元)"+"，小计："+total+"(元)"+"\n");
-    }
-    public Double getTotal(){
-        return total;
-    }
+	public String barcode;  //商品编号
+	private String name;	 //商品名
+	private String unit;     //单位
+	private Double  price;   //商品单价
+	private Double discount; //商品折扣率
+	private int num;        //商品数量
+	private boolean promotion; //是否 "买二赠一"
+	private boolean isVip;    //是否是vip
+	private Double vipDiscount;//vip折扣率
+	public Item(String bar, String nam, String uni, Double pri,Double dis,boolean pro,boolean isV,Double vip){
+		barcode=bar;
+		name=nam;
+		unit=uni;
+		price=pri;
+		discount=dis;
+		promotion=pro;
+		vipDiscount=vip;
+		isVip=isV;
+		num=1;
+	}
+	public void addItemNum(){
+		num++;
+	}
+	public Double getTotal(){ //实际花的钱数
+		if(isVip)                 //vip客户
+			return (price*num*discount*vipDiscount);
+		if(promotion && num>2){ //参加“买二赠一”
+			return (price*(num-num/3));
+		}
+		return (price*num*discount);//普通客户
+	}
+	public Double getSave(){ //节省的钱数
+		return (price*num-getTotal());
+	}
+	public String twoForOne(){
+		if(isVip)
+			return "";
+		if(promotion && num>2){
+			return ("名称："+name+"\t，数量："+(num/3)+unit+"\n");
+		}
+		return "";
+	}
+	public String toString(){
+		return ("名称："+name+"\t,数量："+num+unit+"\t,单价："
+				+String.format("%.2f",price)+"(元)"+"\t,小计："
+				+String.format("%.2f",getTotal())+"(元)"+"\n\n");
+	}
 }
--- src/test/ItemManagerTest.java
+++ src/test/ItemManagerTest.java
@@ -6,23 +6,45 @@ import pos.ItemManager;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Created by hanchao on 2016/1/14.
+ */
 public class ItemManagerTest {
-    ItemManager items;
-
+    ItemManager items,items1,items2;
     @Before
     public void setUp() throws Exception {
-        items=new ItemManager();
-        items.addItem("ITEM000000", "可口可乐", "瓶", 3.00);
-        items.addItem("ITEM000000", "可口可乐", "瓶", 3.00);
-        items.addItem("ITEM000001", "雪碧", "瓶", 3.00);
-        items.addItem("ITEM000004", "电池", "个", 2.00);
-
+         items =new ItemManager("index.txt","users.txt");
+        items.fileInput("./src/test/Item1.txt");
+        items1 =new ItemManager("index.txt","users.txt");
+        items1.fileInput("./src/test/Item2.txt");
+        items2 =new ItemManager("index.txt","users.txt");
+        items2.fileInput("./src/test/Item3.txt");
     }
 
     @Test
     public void testTotal() throws Exception {
-        Double result=3.00*2.0+3.0+2.0;
+        //会员
+        Double result=3.00*0.8*4.0*0.8;
         assertEquals(result,items.total());
-
+        //非会员,非买二赠一
+        Double result1=3.00*0.8*4.0;
+        assertEquals(result1,items1.total());
+        //非会员,买二赠一
+        Double result3=3.00*3.0;
+        assertEquals(result3,items2.total());
     }
+
+    @Test
+    public void testSave() throws Exception {
+        //会员
+        Double result=3.0*4.0-3.00*0.8*4.0*0.8;
+        assertEquals(result,items.save());
+        //非会员,非买二赠一
+        Double result1=3.0*4.0-3.00*0.8*4.0;
+        assertEquals(result1,items1.save());
+        //非会员,买二赠一
+        Double result3=3.00;
+        assertEquals(result3,items2.save());
+    }
+
 }
