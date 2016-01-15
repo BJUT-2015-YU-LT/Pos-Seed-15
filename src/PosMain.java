7156d7e9886bee4931f247595ac9bb73b7afd294
han2016 <1960492622@qq.com>
2016-01-15 14:06:47 +0800

    Delete PosMain.java

--- src/PosMain.java
+++
@@ -1,9 +0,0 @@
-
-public class PosMain {
-
-	public static void main(String[] args) {
-		ItemManager items =new ItemManager("index.txt","users.txt");
-		items.fileInput("Items.txt");
-		System.out.println(items.toString());
-	}
-}
